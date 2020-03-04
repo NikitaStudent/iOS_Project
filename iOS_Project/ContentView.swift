8f3f42f1fda6cce86ee667bb9ce4c6c51a486688
NikitaStudent <NKA_2000_00@mail.ru>
2020-03-04 14:04:44 +0300

    Initial Commit

---
+++ iOS_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Project
+//
+//  Created by Никита Кожевников on 04.03.2020.
+//  Copyright © 2020 beet inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
